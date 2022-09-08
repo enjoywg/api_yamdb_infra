4b7c2409897b1be1ae43a14b8f6c0a906a184c0c
enjoyw <enjoywg@gmail.com>
2022-09-08 12:14:44 +0300

    fix pep8

--- api_yamdb/api/v1/permissions.py
+++ api_yamdb/api/v1/permissions.py
@@ -3,8 +3,8 @@ from rest_framework import permissions
 
 class OwnerAdminModeratorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return(request.method in permissions.SAFE_METHODS
-               or request.user.is_authenticated)
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         if request.method == 'POST' and request.user.is_authenticated:
