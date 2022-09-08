b744bbe2de20d4582ad1df4d82d79cb8ef9e6d5c
enjoyw <enjoywg@gmail.com>
2022-09-08 12:08:27 +0300

    fix pep8

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -2,7 +2,6 @@ import os
 from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from django.conf.global_settings import DEFAULT_FROM_EMAIL
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
